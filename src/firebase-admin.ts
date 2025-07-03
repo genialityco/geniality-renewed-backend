589fcff6f7b71952912e782a02c88b862ca5d392
juanfe <lassojuanfe@gmail.com>
2025-07-03 10:38:55 -0500

    Ajusta con variables de entorno

--- src/firebase-admin.ts
+++ src/firebase-admin.ts
@@ -1,12 +1,29 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 // src/firebase-admin.ts
 import * as admin from 'firebase-admin';
 import * as path from 'path';
+import * as fs from 'fs';
 
-const serviceAccountPath = path.resolve(__dirname, '../firebase-sdk.json');
+let serviceAccount: any;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // Toma la variable de entorno en producción (JSON en una sola línea)
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // Toma el archivo local en desarrollo
+  const serviceAccountPath = path.resolve(__dirname, '../firebase-sdk.json');
+  // Para evitar error si no existe, verifica existencia del archivo
+  if (!fs.existsSync(serviceAccountPath)) {
+    throw new Error(
+      'No se encontró firebase-sdk.json y no se configuró la variable FIREBASE_SERVICE_ACCOUNT',
+    );
+  }
+  serviceAccount = require(serviceAccountPath);
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(require(serviceAccountPath)),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
